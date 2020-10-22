e78549974de3735d5fe96fa1e2c79bcb72caa935
Ross Perry <perryr16@gmail.com>
2020-10-22 14:13:45 -0600

    bypass create -> straight to index -> no db

--- app/controllers/dbless_search_controller.rb
+++ app/controllers/dbless_search_controller.rb
@@ -1,33 +1,34 @@
 class DblessSearchController < ApplicationController
 
   def index
-    @data = session[:search_data]
-    session[:search_data][:page] += 1 if params[:page] == "next"
-    session[:search_data][:page] -= 1 if params[:page] == "back"
-    @page = session[:page]
+    @data = {movies: create_movie_list, search: params[:search], type: params[:search_type], page:0}
+    @data[:page] += 1 if params[:page] == "next"
+    @data[:page] -= 1 if params[:page] == "back"
   end
 
-  def create 
-    session[:search_data] = 
-      {search: params[:search], 
-      type: params[:search_type],
-      page: 0
-      }
-    create_movie_list
-    redirect_to dbless_search_index_path
-  end
+  # def create 
+  #   binding.pry
+  #   session[:search_data] = 
+  #     {search: params[:search], 
+  #     type: params[:search_type],
+  #     page: 0
+  #     }
+  #   # create_movie_list
+  #   redirect_to dbless_search_index_path
+  # end
 
   private 
   
   def create_movie_list
     results = DblessResults.new
     if params[:person_type] == "Actor's"
-      session[:search_data][:movie_list] = results.best_acted(params[:search]) if params[:search_type] == "Best"
-      session[:search_data][:movie_list] = results.worst_acted(params[:search]) if params[:search_type] == "Worst"
+      movies = results.best_acted(params[:search]) if params[:search_type] == "Best"
+      movies = results.worst_acted(params[:search]) if params[:search_type] == "Worst"
     elsif params[:person_type] == "Director's"
-      session[:search_data][:movie_list] = results.best_directed(params[:search]) if params[:search_type] == "Best"
-      session[:search_data][:movie_list] = results.worst_directed(params[:search]) if params[:search_type] == "Worst"
+      movies = results.best_directed(params[:search]) if params[:search_type] == "Best"
+      movies = results.worst_directed(params[:search]) if params[:search_type] == "Worst"
     end
+    movies
   end
   
 end
