5e4f1149e6dede7cd515857a404b885b99494b40
Ross Perry <perryr16@gmail.com>
2020-10-27 15:16:15 -0600

    model tests for user movies

--- spec/features/splash_spec.rb
+++ spec/features/splash_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "splash page" do 
-  xit "has a search field for best movies by actor" do 
+  it "has a search field for best movies by actor" do 
     visit '/'
     within('.movie-search') do 
       select "Actor's"
@@ -9,7 +9,7 @@ describe "splash page" do
       fill_in :search, with: "Brad Pitt"
       click_button "Search"
     end
-    expect(current_path).to eq("/movie_search")
+    expect(current_path).to eq("/dbless_search")
   end
   
   xit 'searches correctly - actor worst' do 
@@ -20,7 +20,7 @@ describe "splash page" do
       fill_in :search, with: "Brad Pitt"
       click_button "Search"
     end
-    expect(current_path).to eq("/movie_search")
+    expect(current_path).to eq("/dblrd_search")
   end
 
   it 'searches incorrectly -  actor worst' do 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_many :user_movies}
+  it {should have_many(:movies).through(:user_movies)}
 end
