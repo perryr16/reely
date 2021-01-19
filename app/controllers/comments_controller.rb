d35646e1c94888417c8df54f0cf943fa66f03c32
Ross Perry <perryr16@gmail.com>
2021-01-19 13:57:10 -0700

    comments diplaying, style needs work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
   def create 
+    Comment.create(
+      comment: params[:comment],
+      image_url: current_user.photo,
+      user_id: current_user.id,
+      movie_id: params[:format]
+    )
+    movie = Movie.find(params[:format])
+    flash[:notice] = "You have commented on #{movie.title}"
+    redirect_back(fallback_location: '/profile')
   end
 
   def new 
