3c9b8f2ff0fd6063c1bdc897031576a71af2459e
Ross Perry <perryr16@gmail.com>
2021-01-21 13:28:12 -0700

    update test suite for streaming results

---
+++ spec/poros/streaming_results_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "results are formatted correctly" do 
+  it "returns data for fightclub" do 
+    fight_club_id = "tt0137523"
+    results = StreamingResults.new.streaming_data(fight_club_id)
+    expect(results[0][:provider]).to be_truthy
+    expect(results[0][:price]).to be_truthy
+    expect(results[0][:url]).to be_truthy
+    expect(results[-1][:provider]).to be_truthy
+    expect(results[-1][:price]).to be_truthy
+    expect(results[-1][:url]).to be_truthy
+
+  end
+
+end
--- spec/services/go_watch_api_spec.rb
+++ spec/services/go_watch_api_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe 'GoWatch service returns steaming data for a movie' do 
-  xit "returns fightclub given id" do
+  it "returns fightclub given id" do
     service = GoWatchService.new
     fight_club_id = "tt0137523"
     results = service.get_streaming_data(fight_club_id)
+    binding.pry
     expect(results[:name]).to be_truthy
     expect(results[:offers]).to be_truthy
     expect(results[:offers][0][:provider]).to be_truthy
