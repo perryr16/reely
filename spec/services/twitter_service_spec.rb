a8a2627f78275d4a6365359e56827ea48e123ee7
Ross Perry <perryr16@gmail.com>
2021-02-03 15:47:58 -0700

    service test functional

--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -6,8 +6,15 @@ describe 'hit the movie database api' do
   end
 
   it 'returns tweets by query' do 
-    x = @service.get_tweets("True Romance")
-    binding.pry
+    body = @service.get_tweets("True Romance")
+    expect(body[:data]).to be_truthy
+    expect(body[:data].length).to eq(10)
+    
+    tweets = body[:data]
+    expect(tweets[0][:id]).to be_truthy
+    expect(tweets[0][:text]).to be_truthy
+    expect(tweets[-1][:id]).to be_truthy
+    expect(tweets[-1][:text]).to be_truthy
 
   end
 
