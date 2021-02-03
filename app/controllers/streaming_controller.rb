6679c4cf33d7c732f4079cc190a3fe27e58c2b1d
Ross Perry <perryr16@gmail.com>
2021-02-03 11:05:45 -0700

    moved message position

--- app/controllers/streaming_controller.rb
+++ app/controllers/streaming_controller.rb
@@ -2,6 +2,7 @@ class StreamingController < ApplicationController
 
   def index
     @movie = Movie.find(params[:movie_id])
+    binding.pry
     @streaming_data = StreamingResults.new.streaming_data(@movie.imdb_id)
   end
 
