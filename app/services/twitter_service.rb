1db384bf6f465c0a02d6de78bda2dd463b42f449
Ross Perry <perryr16@gmail.com>
2021-02-03 16:08:32 -0700

    refactored get_tweets

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -1,8 +1,10 @@
 class TwitterService 
 
   def get_tweets(query)
-    response = conn.get('/2/tweets/search/recent?expansions=author_id&user.fields=name,username,profile_image_url') do |req|
+    response = conn.get('/2/tweets/search/recent') do |req|
       req.params["query"] = query
+      req.params[:expansions] = 'author_id'
+      req.params['user.fields'] = 'name,username,profile_image_url'
     end
     x = JSON.parse(response.body, symbolize_names: true)
     binding.pry
