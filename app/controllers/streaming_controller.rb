15e8b259a4a8219319a0b2ce8e15cd1b9edb10c1
Ross Perry <perryr16@gmail.com>
2021-02-03 11:20:02 -0700

    prep for movie show page

--- app/controllers/streaming_controller.rb
+++ app/controllers/streaming_controller.rb
@@ -2,7 +2,6 @@ class StreamingController < ApplicationController
 
   def index
     @movie = Movie.find(params[:movie_id])
-    binding.pry
     @streaming_data = StreamingResults.new.streaming_data(@movie.imdb_id)
   end
 
