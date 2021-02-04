f9be50b08e3f61c6ac6bbd0e5c1fbe3270ae1982
Ross Perry <perryr16@gmail.com>
2021-02-04 12:28:31 -0700

    remove streaming controller

--- app/controllers/streaming_controller.rb
+++
@@ -1,8 +0,0 @@
-class StreamingController < ApplicationController
-
-  def index
-    @movie = Movie.find(params[:movie_id])
-    @streaming_data = StreamingResults.new.streaming_data(@movie.imdb_id)
-  end
-
-end
