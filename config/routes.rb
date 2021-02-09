aed95230073dd583cd515e1dc8e8b71840782669
Ross Perry <perryr16@gmail.com>
2021-02-09 16:30:29 -0700

    route cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources actor
   get '/', to: 'splash#index'
-  # resources :best_movie_search, only: [:index]
-  # resources :worst_movie_search, only: [:index]
-  resources :movie_search, only: [:create, :index]
+
+  # resources :movie_search, only: [:create, :index]
   resources :dbless_search, only: [:create, :index]
   resources :user_movies, only: [:create, :index, :destroy]
   resources :comments, only: [:create, :destroy]
@@ -13,12 +12,8 @@ Rails.application.routes.draw do
     resources :streaming, only: [:index, :show]
   end
 
-  # get '/movies/:id/streaming', to: 'streaming#show'
-
   get '/movies/:movie_id/comments', to: 'comments#new'
-
   get 'auth/:provider/callback', to: 'sessions#create'
-
   get '/profile', to: 'users#show'
   get '/logout', to: 'sessions#destroy'
   get '/show', to: 'splash#show'
